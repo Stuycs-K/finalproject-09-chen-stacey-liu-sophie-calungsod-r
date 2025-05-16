fd3ba647c878327c635de6bd3be0d19386791ad3
Raechel Grace Calungsod <raegrasc13@gmail.com>
2025-05-16 14:21:13 -0400

    fixed up the main function to take and respond to options, fixed some commenting

--- whitespace.c
+++ whitespace.c
@@ -1,14 +1,21 @@
 #include "whitespace.h"
 
 
-int main(){
-    // RUN A COMMAND THAT READS THE WHITESPACE IF AN OPTION IS USED TO DO THAT, THEN PUT THAT INTO LINE[]
-    // otherwise just print the whitespace
-  // uses function on a string and then calls execvp successfully
-  char * args[256];
-  char line[] = "echo 'Hello World'"; // this line should come from 
-  parse_args( line, args ); // feeds in lines, returns args
-  execvp(args[0], args);
+int main(int argc, char const *argv[]){
+  if (argc<=1){
+    printf("\nUse either option '-p' to print the translated Whitespace or '-r' to run the translated command.");
+  }
+  if (argc>1 && strcmp(argv[1],"-p")==0){ // first argument is 'p', print the translated
+    char* translated = 'hi'; // replace with function call
+    printf(translated);
+  }
+  if (argc>1 && strcmp(argv[1],"-r")==0){ // first argument is 'p', runs the translated command
+    // uses function on a string and then calls execvp successfully
+    char * args[1024*4]; // change later
+    char line[] = "echo 'Hello World'"; // this line should come from 
+    parse_args(line, args); // feeds in lines, returns args
+    execvp(args[0], args);
+  }
 }
 
 // potentially move to a new file later
@@ -23,22 +30,6 @@ void parse_args( char * line, char ** arg_ary ){
   arg_ary[i] = NULL;
 }
 
-
-/*void readFile(char* fileName) {
-    FILE *input = fopen(fileName, "r");
-    if (input==NULL){
-        perror("can't open file");
-    }
-
-    char* space;
-    int read = 
-    while (read(&space, 1, 1, input)==1) { // one byte at a time
-        if(space==' ') printf("space");
-        // do new line stuff
-    }
-    fclose(input);
-}*/
-
 // gets the string of spaces, tabs, and new lines
 char * readFile(char* fileName){
   int file = open(fileName, O_RDONLY , 0);
@@ -48,11 +39,11 @@ char * readFile(char* fileName){
   }
 
   char* buff = malloc(1024*4); // make files later, see if we need more
-  // error msg if the malloc didnt work
+  // add error msg if the malloc didnt work
   int len = 0; // used to go through the buffer
 
   char space;
-  while(read(file,&space,1)==1){ // while there's still a space to read
+  while(read(file,&space,1)==1){ // while there's something to read
     if(space==' ' || space=='\n' || space=='\t'){
         len+=1;
         buff[len]=space;
