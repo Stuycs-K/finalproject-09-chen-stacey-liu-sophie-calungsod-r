bf723533f5972377d732ff5d4bbaf2f6af6bbef2
STACEY_CHEN <schen50@stuy.edu>
2025-05-16 14:46:56 -0400

    worked on numbers for whitespace

---
+++ numbertest.c
@@ -0,0 +1,20 @@
+#include "numbertest.h"
+
+int main(){
+
+}
+
+int findNumber(char* str){
+  int num = 0;
+  char * ptr = str;
+  while (&ptr != '\n'){
+    if (c == ' '){
+      num << 1;
+    }
+    else if (c == '\t'){ // does it need to be ascii?
+      num << 1;
+      num+=;
+    }
+    ptr++;
+  }
+}
---
+++ numbertest.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#include <ctype.h>
+#include <signal.h>
+
+#ifndef numbertest
+#define numbertest
+
+#endif
