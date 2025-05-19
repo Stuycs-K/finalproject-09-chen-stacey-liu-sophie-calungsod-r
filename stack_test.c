1fd5363ac8e942f5f425ecc4ffc23240c0da0b1d
Sophie Liu <sliu50@stuy.edu>
2025-05-19 14:32:10 -0400

    finished rest of stack manipulation functions and fixed some of the previously coded ones

--- stack_test.c
+++ stack_test.c
@@ -2,39 +2,82 @@
 
 #define MAX_STACK_SIZE 100
 
-struct {
+typedef struct {
   int ary[MAX_STACK_SIZE];
-  int top = -1;
+  int top;
 } Stack;
 
 void init(Stack *stack) {
-
+  stack->top = -1;
 }
 
+/*Returns 1 if the stack is empty, 0 if not*/
 int isEmpty(Stack *stack) {
-  return stack->top == -1;
+  return (stack->top == -1);
 }
 
 int isFull(Stack *stack) {
   return stack->top == MAX_STACK_SIZE - 1;
 }
 
+/*Push number n onto stack*/
 void push(Stack *stack, int n) {
-  if (isFull) {
+  if (isFull(stack)) {
     perror(":(");
-    return 0;
+    return;
   }
-  top++;
+  stack->top++;
   stack->ary[stack->top] = n;
 }
 
+/*Duplicate top item on stack*/
 void duplicate(Stack *stack) {
-  if (isFull || isEmpty) {
+  if (isFull(stack) || isEmpty(stack)) {
     perror(":(");
-    return 0;
+    return;
   }
-  top++;
-  stack->ary[stack->top] = stack->ary[stack->top--];
+  stack->top++;
+  stack->ary[stack->top] = stack->ary[stack->top-1];
+}
+
+/*Swap the top 2 items on stack*/
+void swap(Stack *stack) {
+  if (isEmpty(stack)) {
+    perror(":(");
+    return;
+  }
+  int temp = stack->ary[stack->top];
+  stack->ary[stack->top] = stack->ary[stack->top-1];
+  stack->ary[stack->top-1] = temp;
+}
+
+/*Discard top item on stack*/
+void discard(Stack *stack) {
+  if (isEmpty(stack)) {
+    perror(":(");
+    return;
+  }
+  stack->top--;
+}
+
+/*Copy nth item on the stack to top of stack*/
+void copy(Stack *stack, int n) {
+  if (isFull(stack) || isEmpty(stack)) {
+    perror(":(");
+    return;
+  }
+  stack->top++;
+  stack->ary[stack->top] = stack->ary[n];
+}
+
+/*Slide (pop and discard) n items off the stack, keeping top item*/
+void slide(Stack *stack, int n) {
+  if (isEmpty(stack)) {
+    perror(":(");
+    return;
+  }
+  stack->ary[0] = stack->ary[stack->top];
+  stack->top = 0;
 }
 
 int main() {
