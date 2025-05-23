2c548371b9229f03346c5e14b9be8aece3e0d7a4
STACEY_CHEN <schen50@stuy.edu>
2025-05-23 14:34:20 -0400

    transferred flow control functions to another file

---
+++ flowcontrol.h
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#include <ctype.h>
+#include <signal.h>
+
+#ifndef flowcontrol
+#define flowcontrol
+char * findLabel(char * ptr);
+void markLoc(char ** label_ary, char* label, char* ptr);
+void callSubRoutine(char ** label_ary, char * label, char* ptr);
+void unCondJump(char * label, char ** label_ary, char ** currPtr);
+void zeroJump(Stack *stack, char * label);
+void negJump(Stack *stack, char * label);
+#endif
